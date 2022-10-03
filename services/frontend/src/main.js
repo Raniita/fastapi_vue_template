6b657f88bf6bdecf23ebb5025a57b3c11d94e45c
Raniita <Raniita@users.noreply.github.com>
2022-10-03 19:04:38 +0200

    vuex done

--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -1,9 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import axios from 'axios';
 
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = 'http://localhost:5000/';  // Endpoint fastapi backend
@@ -12,5 +13,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app');
